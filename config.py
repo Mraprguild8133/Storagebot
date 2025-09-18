8df709aee73f31a58612b1ab5ed4f0423e74d62a
Mraprguild8133 <mraprguild06@gmail.com>
2025-09-18 23:47:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", "1234567"))
 API_HASH = os.getenv("API_HASH", "your_api_hash")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+ADMIN_ID = os.environ.get("ADMIN_ID")
 
 # --- Wasabi Storage ---
 WASABI_ACCESS_KEY = os.getenv("WASABI_ACCESS_KEY", "your_wasabi_access_key")
@@ -16,10 +17,5 @@ WASABI_BUCKET = os.getenv("WASABI_BUCKET", "your-bucket-name")
 WASABI_REGION = os.getenv("WASABI_REGION", "ap-northeast-1")  
 WASABI_ENDPOINT_URL = os.getenv("WASABI_ENDPOINT_URL", "https://s3.wasabisys.com")
 
-# --- Optional Backup Channel ---
-STORAGE_CHANNEL_ID = int(os.getenv("STORAGE_CHANNEL_ID", "-1001234567890"))
-
-# --- In-memory Database ---
-# This will reset every time the bot restarts.
-# Consider replacing with a persistent solution (SQLite, JSON file, etc.)
-FILE_DATABASE = {}
+# Welcome image URL (you can replace this with your own image)
+WELCOME_IMAGE_URL = "https://raw.githubusercontent.com/Mraprguild8133/Telegramstorage-/refs/heads/main/IMG-20250915-WA0013.jpg"
